3e72cacc2c45799d206c13c7a6e4511cb5f25ebe
sandeep perkari <sandeepperkari@sandeeps-MacBook-Pro.local>
2014-05-28 21:46:06 -0400

    implemented start  date time search control

--- Joyride/addSearchViewController.h
+++ Joyride/addSearchViewController.h
@@ -16,15 +16,26 @@
 
 @end
 
-@interface addSearchViewController : UITableViewController
+@interface addSearchViewController : UITableViewController<UITextFieldDelegate>
 
 @property (nonatomic,weak)id<addSearchViewControllerDelegate>delegate;
-
+@property (assign) BOOL datePickerIsShowing;
 @property (nonatomic,strong) IBOutlet UITextField *startingPoint;
-@property (nonatomic,strong) IBOutlet UITextField *destinationPoint;
-
+@property (strong, nonatomic) IBOutlet UITextField *destinationPoint;
+@property(nonatomic,strong) IBOutlet UILabel *StartDateTime;
+@property(nonatomic,strong) IBOutlet UIDatePicker *startDatePicker;
+@property(nonatomic,strong) IBOutlet UIDatePicker *endDatePicker;
+@property (strong, nonatomic) IBOutlet UITableViewCell *startDatePickerCell;
+@property (strong, nonatomic) IBOutlet UITableViewCell *endDatePickerCell;
+@property (nonatomic,strong) NSDateFormatter *dateFormatter;
+@property(nonatomic,strong) IBOutlet  UILabel *startDateTimeLabel;
+@property (strong, nonatomic) IBOutlet UILabel *endDateTimeLabel;
+@property(nonatomic,strong) NSDate *selectedStartDateTime;
+@property(nonatomic,strong) UITextField *activeTextField;
 
 -(IBAction)cancel:(id)sender;
 -(IBAction)search:(id)sender;
+-(IBAction)startDatePickerChange:(UIDatePicker *)sender;
+-(IBAction)endDatePickerChange:(UIDatePicker *)sender;
 
 @end
--- Joyride/addSearchViewController.m
+++ Joyride/addSearchViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "addSearchViewController.h"
+#define kDatePickerIndex 2
+#define kDatePickerCellHeight 164
 
 @interface addSearchViewController ()
 
@@ -14,26 +16,19 @@
 
 @implementation addSearchViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
+
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    //self.startingPoint.text=@" ";
+    self.startingPoint.delegate=self;
+    [self setupDefaultStartDate];
+    [self signUpForKeyboardNotifications];
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+
 
 -(IBAction)cancel:(id)sender
 {
@@ -45,4 +40,111 @@
     [self.delegate addSearchViewControllerDidSearch:self];
     
 }
+
+
+-(void)setupDefaultStartDate
+{
+    self.dateFormatter=[[NSDateFormatter alloc] init];
+    [self.dateFormatter setDateStyle:NSDateFormatterMediumStyle];
+    [self.dateFormatter setTimeStyle:NSDateFormatterNoStyle];
+    
+    NSDate *defaultDate=[NSDate date];
+    
+    self.startDateTimeLabel.text=[self.dateFormatter stringFromDate:defaultDate];
+    self.startDateTimeLabel.textColor=[self.tableView tintColor];
+    self.selectedStartDateTime=defaultDate;
+    
+
+}
+
+
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    CGFloat height=self.tableView.rowHeight;
+    
+    if(indexPath.row==kDatePickerIndex)
+    {
+        height=self.datePickerIsShowing ?  kDatePickerCellHeight:0.0f;
+        
+    }
+    return height;
+}
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if(indexPath.row==1)
+    {
+        if(self.datePickerIsShowing)
+        {
+            [self hideDatePickerCell];
+        }
+        else
+        {
+            [self.activeTextField resignFirstResponder];
+            [self showDatePickerCell];
+        }
+    }
+    [self.tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+-(void)showDatePickerCell
+{
+    
+    self.datePickerIsShowing=YES;
+    [self.tableView beginUpdates];
+    [self.tableView endUpdates];
+    self.startDatePicker.hidden=NO;
+    self.startDatePicker.alpha=0.0f;
+    
+    [UIView animateWithDuration:0.25 animations:^{
+    
+        self.startDatePicker.alpha=1.0f;
+    
+    }];
+
+}
+-(void)hideDatePickerCell
+{
+    self.datePickerIsShowing=NO;
+    [self.tableView beginUpdates];
+    [self.tableView endUpdates];
+    
+    [UIView animateWithDuration:0.25
+    animations:^{
+        self.startDatePicker.alpha=0.0f;
+        
+    }
+    completion:^(BOOL finished)
+    {
+        self.startDatePicker.hidden=YES;
+                     
+    }];
+    
+
+}
+-(IBAction)startDatePickerChange:(UIDatePicker *)sender
+{
+    self.startDateTimeLabel.text= [self.dateFormatter stringFromDate:sender.date];
+    self.selectedStartDateTime=sender.date;
+    
+
+}
+- (void)signUpForKeyboardNotifications
+{
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillShow) name:UIKeyboardWillShowNotification object:nil];
+    
+}
+- (void)keyboardWillShow {
+    
+    if (self.datePickerIsShowing){
+        
+        [self hideDatePickerCell];
+    }
+}
+
+-(void)textFieldDidBeginEditing:(UITextField *)textField
+{
+    self.activeTextField=textField;
+}
+
 @end
